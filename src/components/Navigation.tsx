100c835df436d58553e90f730d4384db862ecbc4
kkumber <angelbertreyes28@gmail.com>
2025-01-22 13:33:04 +0800

    init tailwind

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav>
-        <Link to='/'>Homepage</Link>
-        <Link to='/register'>Register</Link>
-        <Link to='/login'>Login</Link>
+    <nav className="flex justify-between items-center">
+      {/* Navigations */}
+      <div className=" flex justify-between items-center">
+          <Link to='/' className=""><b>RELIGHT</b></Link>
+          <Link to='/'>Homepage</Link>
+      </div>
+      {/* Authorizations */}
+      <div className="">
+        <Link to='/login'>Sign in</Link>
+      </div>
     </nav>
   );
 };
