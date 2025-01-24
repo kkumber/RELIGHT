94df0ebba78267fc2a50c9b979ea1139bd1afc64
kkumber <angelbertreyes28@gmail.com>
2025-01-24 02:43:38 -0800

    the browse section now sorts the lists based on the button

--- src/pages/Subpages/AddBookPage.tsx
+++ src/pages/Subpages/AddBookPage.tsx
@@ -3,8 +3,8 @@ import Footer from "../../components/layout/Footer";
 
 const AddBookPage = () => {
     return ( 
-        <div className="min-h-screen flex flex-col">        
-        <section className="justify-center items-center flex flex-grow mx-auto w-1/2">
+        <div className="min-h-screen flex flex-col gap-y-20">        
+        <section className="justify-center items-center flex flex-grow m-auto w-1/2">
             <AddBookForm />
         </section>
         <Footer />
--- src/pages/Subpages/Browse.tsx
+++ src/pages/Subpages/Browse.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 const Browse = () => {
     const {data, isLoading, error, fetchData} = useFetch();
     const [bookList, setBookList] = useState<FetchData>();
-    const [query, setQuery] = useState<string>();
+    const [query, setQuery] = useState<string>('title');
 
 
     useEffect(() => {
@@ -19,22 +19,28 @@ const Browse = () => {
         fetchData(`library/books/?sort_by=${query}`)
     }, [query])
 
+    useEffect(() => {
+        if (data) {
+            setBookList(data);
+        }
+    }, [data])
+
     
     return ( 
         <div className="min-h-screen flex flex-col">
             <Header />
             {/* Main Container */}
-            <div className="flex flex-grow">
+            <div className="flex flex-grow flex-col">
                 <nav>
-                    <ul className="flex gap-4 items-center ">
-                        <li><button onClick={() => setQuery('popular')}>Popular</button></li>
-                        <li><button onClick={() => setQuery('explore')}>Explore</button></li>
+                    <ul className="flex gap-8 items-center ">
+                        <li><button onClick={() => setQuery('likes')}>Popular</button></li>
+                        <li><button onClick={() => setQuery('views')}>Explore</button></li>
                         <li><button onClick={() => setQuery('upload_date')}>What's New</button></li>
                         <li><button onClick={() => setQuery('title')}>A-Z List</button></li>
                     </ul>
                     <hr className="bg-primaryRed p-[2px] mb-4 rounded-full"/>
                 </nav>
-                <section className="flex flex-wrap gap-8">
+                <section className="flex flex-wrap gap-12">
                     {
                         bookList?.results.map(book => 
                             <div key={book.id} className="h-full w-28 md:w-40">
