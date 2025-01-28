77d427144d347b8a89be26067e4a1cc85d154f9d
kkumber <angelbertreyes28@gmail.com>
2025-01-28 13:55:22 +0800

    ?

--- src/components/Forms/SearchForm.tsx
+++ src/components/Forms/SearchForm.tsx
@@ -2,18 +2,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchForm = () => {
-
-    const handleSubmit = () => {
-
-    };
+  const handleSubmit = () => {};
   return (
     <form onSubmit={handleSubmit} className="flex">
-        <button className="bg-black py-[3px] px-2 rounded-l-md border-primaryRed">
-            <FontAwesomeIcon icon={faSearch} size="sm" style={{color: 'white'}}/>
-        </button>
-        <input type="search" name="search" placeholder="Search..." className="p-1 border-[1px] rounded-r-2xl border-black focus:outline-none text-sm"/>
+      <button className="bg-black py-[3px] px-2 rounded-l-md border-primaryRed">
+        <FontAwesomeIcon icon={faSearch} size="sm" style={{ color: "white" }} />
+      </button>
+      <input
+        type="search"
+        name="search"
+        placeholder="Search..."
+        className="p-1 border-[1px] rounded-r-2xl border-black focus:outline-none text-sm"
+      />
     </form>
   );
 };
 
-export default SearchForm
+export default SearchForm;
