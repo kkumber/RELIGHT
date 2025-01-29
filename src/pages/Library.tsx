123b3746321aa43eb4d563c46917d6a3b346e930
kkumber <angelbertreyes28@gmail.com>
2025-01-28 20:28:45 -0800

    stash

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -2,11 +2,7 @@ import RenderBooks from "../components/Renders/RenderBooks";
 import Footer from "../components/layout/Footer";
 
 const Library = () => {
-  return (
-    <main>
-        
-    </main>
-  );
+  return <main></main>;
 };
 
 export default Library;
