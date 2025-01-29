0c227f4313f64b97ac8f6819bf2c93f38fbcd352
kkumber <angelbertreyes28@gmail.com>
2025-01-29 02:24:47 -0800

    loading animation

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,6 +1,9 @@
 const Loading = () => {
   return (
-    <div className="">Loading...</div>
+    <div className="flex justify-center items-center h-full">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-primaryRed rounded-full animate-spin"></div>
+      <span>Loading...</span>
+    </div>
   );
 };
 
