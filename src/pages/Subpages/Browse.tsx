6fa1090e9d12deeb4d9badeca4d56ffa40409e84
kkumber <angelbertreyes28@gmail.com>
2025-02-11 21:57:12 +0800

    added tailwind plugin scrollbar-hide

--- src/pages/Subpages/Browse.tsx
+++ src/pages/Subpages/Browse.tsx
@@ -28,55 +28,58 @@ const Browse = () => {
     <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-[#121212]">
       <Header />
       {/* Main Container */}
-      <div className="flex flex-grow flex-col w-9/12 mx-auto mb-20 bg-white p-8 rounded-lg dark:bg-[#1E1E1E]">
+      <div className="flex flex-grow flex-col w-full md:w-9/12 mx-auto mb-20 bg-white px-2 py-8 md:p-8 rounded-lg dark:bg-[#1E1E1E]">
         <nav>
-          <ul className="flex gap-8 items-center font-bold">
-            <li>
-              <button
-                onClick={() => setQuery("views")}
-                className={`${
-                  query === "views" ? "bg-primaryRed" : undefined
-                } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
-              >
-                Popular
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => setQuery("?")}
-                className={`${
-                  query === "?" ? "bg-primaryRed" : undefined
-                } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
-              >
-                Explore
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => setQuery("upload_date")}
-                className={`${
-                  query === "upload_date" ? "bg-primaryRed" : undefined
-                } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
-              >
-                What's New
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => setQuery("title")}
-                className={`${
-                  query === "title" ? "bg-primaryRed" : undefined
-                } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
-              >
-                A-Z List
-              </button>
-            </li>
-          </ul>
+          {/* Wrapper div enables horizontal scrolling on mobile */}
+          <div className="overflow-x-auto scrollbar-hide">
+            <ul className="flex gap-8 items-center font-bold whitespace-nowrap">
+              <li>
+                <button
+                  onClick={() => setQuery("views")}
+                  className={`${
+                    query === "views" ? "bg-primaryRed" : undefined
+                  } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
+                >
+                  Popular
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => setQuery("?")}
+                  className={`${
+                    query === "?" ? "bg-primaryRed" : undefined
+                  } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
+                >
+                  Explore
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => setQuery("upload_date")}
+                  className={`${
+                    query === "upload_date" ? "bg-primaryRed" : undefined
+                  } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
+                >
+                  What's New
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => setQuery("title")}
+                  className={`${
+                    query === "title" ? "bg-primaryRed" : undefined
+                  } py-1 px-4 rounded-full transition-all duration-300 linear hover:text-black/40 dark:hover:text-white/40`}
+                >
+                  A-Z List
+                </button>
+              </li>
+            </ul>
+          </div>
           <div className="h-[2px] bg-primaryRed/80 w-full my-4 rounded-full" />
         </nav>
-        <section className="flex flex-wrap gap-4">
+        <section className="flex flex-wrap gap-4 md:gap-8">
           {bookList?.results.map((book) => (
-            <div key={book.id} className="h-full w-28 md:w-40">
+            <div key={book.id}>
               <RenderBooks book={book} />
             </div>
           ))}
@@ -87,12 +90,14 @@ const Browse = () => {
       <div className="flex justify-between items-center w-9/12 m-auto">
         <button
           onClick={() => fetchData(data.next)}
+          disabled={!bookList?.previous}
           className="bg-transparent py-2 px-4 rounded-full text-primaryRed font-semibold hover:text-primaryRed/50"
         >
           Previous
         </button>
         <button
           onClick={() => fetchData(data.previous)}
+          disabled={!bookList?.next}
           className="bg-transparent py-2 px-4 rounded-full text-primaryRed font-semibold hover:text-primaryRed/50"
         >
           Next
