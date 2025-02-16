70117d7e2cdb472b2571a159b093cb9f4a046f40
kkumber <angelbertreyes28@gmail.com>
2025-02-15 16:40:20 -0800

    save

--- src/pages/Subpages/Details.tsx
+++ src/pages/Subpages/Details.tsx
@@ -144,16 +144,22 @@ const Details = () => {
                   </div>
                   <p>Upload Date: {book.upload_date} </p>
                   <p>Uploaded by: {book.uploaded_by} </p>
-                  <form onSubmit={handleBookmark}>
-                    <button className="bg-primaryRed py-2 px-4 rounded-lg text-white font-semibold hover:bg-primaryRed/80">
-                      {book.likes && book.likes.includes(user?.id)
-                        ? "Remove from Library"
-                        : "Add to Library"}
+                  {/* Buttons Container */}
+                  <div className="flex justify-content items-center gap-4">
+                    <form onSubmit={handleBookmark}>
+                      <button className="bg-primaryRed py-2 px-4 rounded-lg text-white font-semibold hover:bg-primaryRed/80">
+                        {book.likes && book.likes.includes(user?.id)
+                          ? "Remove from Library"
+                          : "Add to Library"}
+                      </button>
+                    </form>
+                    <button
+                      onClick={() => handleReadNavigate(book.pdf_file)}
+                      className="bg-primaryRed py-2 px-4 rounded-lg text-white font-semibold hover:bg-primaryRed/80"
+                    >
+                      Read
                     </button>
-                  </form>
-                  <button onClick={() => handleReadNavigate(book.pdf_file)}>
-                    Read
-                  </button>
+                  </div>
                 </div>
               </div>
             </div>
